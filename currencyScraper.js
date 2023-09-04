6e6cc2b75fcc59e4a7c90e520bf3b9b414e02364
Rohan Phulkar <rohanphulkar936@gmail.com>
2023-09-04 16:47:48 +0530

    forex tracker done

---
+++ currencyScraper.js
@@ -0,0 +1,32 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+// Define the base and target currency codes
+async function scrapeExchangeRate(toCurrency) {
+  const url = `https://www.xe.com/currencyconverter/convert/?Amount=1&From=INR&To=${toCurrency.toUpperCase()}`;
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+    const $ = cheerio.load(response.data);
+
+    // Extract the exchange rate from the website
+    const exchangeRate = $(".iGrAod").text();
+
+    // Clean and format the exchange rate
+    const cleanedRate = exchangeRate.replace(/[^0-9.]/g, ""); // Remove non-numeric characters
+
+    if (cleanedRate) {
+      return cleanedRate;
+    } else {
+      return "Error";
+    }
+  } catch (error) {
+    return "Error";
+  }
+}
+
+module.exports = scrapeExchangeRate;
---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const scrapeExchangeRate = require("./currencyScraper.js"); // Require the function
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.get("/convert/:currency", async (req, res) => {
+  try {
+    const { currency } = req.params;
+
+    if (!currency) {
+      return res.status(400).json({ error: "Missing currency code" });
+    }
+
+    const exchangeRate = await scrapeExchangeRate(currency);
+
+    if (exchangeRate === "Error") {
+      return res.status(500).json({ error: "Error scraping exchange rate" });
+    }
+
+    res.json({
+      currency,
+      exchangeRate,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
